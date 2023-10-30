7f07e31266869b9cb9894695a11d50a57dd174bb
Thana B <thana.b@g-gen.co.jp>
2023-10-30 15:30:03 +0900

    refactor/remove unused function

--- modules/utils.py
+++ modules/utils.py
@@ -22,18 +22,3 @@ def serialize_to_pickle(python_object):
     """
     serialized_object = pickle.dumps(python_object)
     return serialized_object
-
-
-def check_repetitive_string(input_string):
-    """
-    Check if the same text is repeated 2 or more times
-    """
-    # Remove numbers and newline characters from the input string
-    input_string = re.sub(r"\d+", "", input_string).replace("\n", "")
-
-    for i in range(1, len(input_string) // 2 + 1):
-        pattern = "(.{" + str(i) + ",}?)\\1{1,}"
-        match = re.search(pattern, input_string)
-        if match:
-            return True
-    return False

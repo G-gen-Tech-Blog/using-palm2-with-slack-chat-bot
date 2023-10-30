42df91cb3aea25c090ec3dad66248c1d25386abb
Thana B <thana.b@g-gen.co.jp>
2023-10-30 11:21:11 +0900

    fast api text model

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-import json
-import pickle
-
 from fastapi import FastAPI, Request
 from google.cloud import logging
 
@@ -14,7 +11,7 @@ from vertexai.language_models import ChatModel, InputOutputTextPair, TextGenerat
 
 from modules import gc_utils, utils
 
-# 環境変数を読み込む
+# Secret Managerから環境変数を読み込む（Secret Managerを使わなければ１．事前に環境変数にこれらの値を格納し、環境変数から読み込む。２.ハードコードで入力。）
 PROJECT_ID, PROJECT_NO = gc_utils.get_project_number()
 SIGNING_SECRET = gc_utils.access_secret_version(
     PROJECT_NO, "palm2-slack-chatbot-l-signing-secret"
@@ -40,7 +37,6 @@ async def endpoint(req: Request):
 # VertexAIを初期化
 vertexai.init(project=PROJECT_ID, location=RESOURCE_LOCATION)
 
-chat_model = ChatModel.from_pretrained("chat-bison@001")
 text_model = TextGenerationModel.from_pretrained("text-bison@001")
 PARAMETERS = {
     "max_output_tokens": 1024,
@@ -60,25 +56,10 @@ logger_name = "palm2_slack_chatbot"
 # cloud logging: ロガーを選択する
 logger = logging_client.logger(logger_name)
 
-# 入力出力例を準備
-sample_raws = []
-with open("./samples/sample_input-output_pairs.jsonl", "r", encoding="utf-8-sig") as f:
-    for line in f:
-        sample_raws.append(json.loads(line))
-
-examples = []
-for item in sample_raws:
-    example_pair = InputOutputTextPair(
-        input_text=item["input_text"],
-        output_text=item["output_text"],
-    )
-    examples.append(example_pair)
-
-
 # 本動作はここから
 
 
-def post_message_if_not_from_bot(
+def generate_response(
     client: AsyncWebClient,
     ts: str,
     conversation_thread: str,
@@ -100,32 +81,7 @@ def post_message_if_not_from_bot(
     prompt : str
         プロンプト
     """
-
-    # google cloud storage で保存したチャット履歴のオブジェクト名を指定
-    historical_chat_blob_name = f"{conversation_thread}.pkl"
-
-    # google cloud storage で保存したチャット履歴のオブジェクトを取得
-    historical_chat_blob = gc_utils.download_blob(
-        HISTORICAL_CHAT_BUCKET_NAME, historical_chat_blob_name
-    )
-    is_existing_thread = historical_chat_blob.exists()
-
-    message_history = []
-    if is_existing_thread:
-        # チャット履歴が存在すれば、過去のチャット履歴をダウンロードし、chat_model に投入し、チャットのセッション再開
-
-        serialized_historical_chat = historical_chat_blob.download_as_bytes()
-        # 履歴のオブジェクトを逆シリアル化
-        historical_chat = pickle.loads(serialized_historical_chat)
-        message_history = historical_chat["historical_chat"]
-
-    this_prompt_context = f"{RESPONSE_STYLE}"
-    chat = chat_model.start_chat(
-        context=this_prompt_context,
-        examples=examples,
-        message_history=message_history,
-    )
-    response = chat.send_message(prompt, **PARAMETERS)
+    response = text_model.predict(prompt, **PARAMETERS)
 
     # ブロックされたか確認する
     is_blocked = response.is_blocked
@@ -140,16 +96,8 @@ def post_message_if_not_from_bot(
     # レスポンスをslackへ返す
     client.chat_postMessage(channel=channel_id, thread_ts=ts, text=payload)
 
-    gc_utils.store_historical_chat_to_gcs(
-        """dummy_metadata_chat""",
-        chat._message_history,
-        HISTORICAL_CHAT_BUCKET_NAME,
-        historical_chat_blob_name,
-    )
-
-    if not is_existing_thread:
-        keyword = gc_utils.get_keyword(text_model, prompt, PARAMETERS)
-        gc_utils.send_log(logger, user_id, prompt, payload, keyword)
+    keyword = gc_utils.get_keyword(text_model, prompt, PARAMETERS)
+    gc_utils.send_log(logger, user_id, prompt, payload, keyword)
 
 
 @app.event("message")
@@ -168,6 +116,4 @@ def handle_incoming_message(client: AsyncWebClient, payload: dict) -> None:
     ts = payload.get("ts")
     thread_ts = payload.get("thread_ts")
     conversation_thread = ts if thread_ts is None else thread_ts
-    post_message_if_not_from_bot(
-        client, ts, conversation_thread, user_id, channel_id, prompt
-    )
+    generate_response(client, ts, conversation_thread, user_id, channel_id, prompt)
